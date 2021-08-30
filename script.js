df2b94d2379a75ee230db3e32e7a11ca4b377603
ronin-4422 <mmfesenko@gmail.com>
2021-08-30 22:04:20 +0300

    did all tasks

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 console.log("----------------Task 3")
-async function delay() {
-  let promise1 =  new Promise(function(resolve, reject) {
-    setTimeout(() => resolve('Hello!'), 1000);
-   });
-   let result1 = await promise1;
-   console.log(result1)
-};
+function timeout(data, ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      (resolve(data))
+    },ms)
+  });
+}
+timeout({name:'user'}, 1000).then((data) => console.log('Hello!', data));
+
+// async function delay() {
+//   let promise1 =  new Promise(function(resolve, reject) {
+//     setTimeout(() => resolve('Hello!'), 1000);
+//    });
+//    let result1 = await promise1;
+//    console.log(result1)
+// };
 
 
 console.log("----------------Task 4")
 async function getUserInfo() {
   let promise2 =  new Promise(function(resolve, reject) {
-  setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 } ), 1000);
+  timeout(resolve({ name: 'Vic', age: 21, id: 1 }), 3000);
  });
  let result2 = await promise2;
- console.log(result2)
+ console.log(result2);
 }
 
 async function getUserAvatar() {
   let promise3 = new Promise(function(resolve, reject) {
-    let avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg'
-    setTimeout(() => resolve(avatar), 1000);
+    let avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg';
+    timeout(resolve(avatar), 3000);
   });
   let result3 = await promise3;
   console.log(result3);
@@ -30,13 +39,12 @@ async function getUserAvatar() {
 async function getUserAdditionalInfo() {
   let promise4 = new Promise(function(resolve, reject) {
     let interests = ['sport', 'books'];
-    setTimeout(() => resolve(interests), 1000);
+    timeout(resolve(interests), 3000);
   });
   let result4 = await promise4;
-  console.log(result4)
+  console.log(result4);
 }
 async function getResult() { 
-  await delay();
   await getUserInfo();
   await getUserAvatar();
   await getUserAdditionalInfo();
